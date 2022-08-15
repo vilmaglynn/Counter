551c837eb733303d9069cbdc9531621f8a6a8950
Vilma Hernandez <vilmaglynn@hotmail.com>
2022-08-15 07:01:34 +0100

    allchanges

---
+++ app.js
@@ -0,0 +1,36 @@
+//set initial count
+let count =0;
+
+//select value and buttons
+const value = document.querySelector("#value");
+const btns = document.querySelectorAll(".btn");
+
+btns.forEach(function (btn) {
+	btn.addEventListener("click", function(e){
+		const styles = e.currentTarget.classList;
+		if (styles.contains('decrease')){
+			count--;
+		}
+		
+		if (styles.contains('increase')){
+			count++;
+		}
+
+		if (styles.contains('reset')){
+			count=0;
+		}
+		if (count > 0) {
+			value.style.color = "green";
+		}
+
+		if (count < 0) {
+			value.style.color = "red";
+		}
+		if (count === 0) {
+			value.style.color = "black";
+		}
+		value.textContent = count;
+	
+});
+
+});
